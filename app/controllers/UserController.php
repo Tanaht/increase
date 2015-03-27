714f04e0baf1eefc7c52bd2a4de6cb8a38f254b6
Antoine Charpentier <a.charpentier10@laposte.net>
2015-03-27 19:43:39 +0100

    Bouton Ouvrir... ->fonction JQuery.load() utilisé (Ajax) Ajout de la seconde IHM

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -8,10 +8,21 @@ class UserController extends \Phalcon\Mvc\Controller
 
     }
 
-    public function projectsAction($id)
+    public function projectsAction($idUser)
     {
-        $user = User::findFirst("id = $id");
+        $user = User::findFirst("id = $idUser");
         $this->view->setVar("user", $user);
+
+
+        $this->jquery->click("div.projet button", "
+            $('body').load('../../user/project/' + $(this).data('href'));
+        ");
+        $this->jquery->compile($this->view);
+    }
+
+    public function projectAction($idProjet){
+        $projet = Projet::findFirst("id = $idProjet");
+        $this->view->setVar("projet", $projet);
     }
 
 }
